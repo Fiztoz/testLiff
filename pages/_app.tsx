6db5b98d3e2c20116a6a631dc1151380fe11d874
PEA\513238 <hm.hanafee@gmail.com>
2023-03-01 09:56:25 +0700

    m

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { useEffect } from 'react'
+// import {liff} from '@line/liff'
+const liffId = "1660679590-jV8GGlze"
+
 export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect( () => {
+
+    const Liff = async () => {
+    const liff = (await import('@line/liff')).default
+    try{
+      if(liffId !== undefined){
+        await liff.init({ liffId })
+      }
+    }catch (error) {
+        console.error('liff init error', error)
+      }
+    if(!liff.isLoggedIn()){
+      liff.login()
+    }
+    }
+    Liff()
+  }, []);
+
+
   return <Component {...pageProps} />
 }
